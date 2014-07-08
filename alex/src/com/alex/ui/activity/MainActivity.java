086fedba76d6c5ef28ae773e209e80874f104f63
alex <chang.5241@gmail.com>
2014-07-09 00:38:30 +0800

    ui library

--- alex/src/com/alex/ui/activity/MainActivity.java
+++ alex/src/com/alex/ui/activity/MainActivity.java
@@ -1,27 +1,19 @@
 package com.alex.ui.activity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-import butterknife.ButterKnife;
-import butterknife.InjectView;
+import com.alex.ui.widget.StaggeredGridView.StaggeredGridView;
 
 
 public class MainActivity extends BaseActivity {
     /**
      * Called when the activity is first created.
      */
-    @InjectView(R.id.tv)
-    TextView tv;
-    @InjectView(R.id.button)
-    Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ButterKnife.inject(this);
-        button.setText("bbb");
-        tv.setText("ttt");
+        StaggeredGridView gridView=new StaggeredGridView();
+        gridView.setonsc
     }
 }
---
+++ alex/src/com/alex/ui/activity/WebViewActivity.java
@@ -0,0 +1,15 @@
+package com.alex.ui.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by changxuebo on 14-7-8.
+ */
+public class WebViewActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
